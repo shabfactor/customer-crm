dec83467f24d8708169e0417df0679e67e297c1e
Shaibal chakraborty <shaibal.chakraborty@merlot.aero>
2022-01-11 17:02:27 +1300

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Starter Antd Admin (Crud & auth) Mern App (Express.js / React / Redux / MongoDB)
+
+```
+Don't forget please to ⭐ this repo if you like this Starter Antd Admin!
+```
+
+
+![starter-antd-admin-crud-auth-mern-crud-auth](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/opp4yj177dizyaosah0o.png)
+
+
+## CRM Starter Mern Antd Admin App 
+
+I build Starter Crm App based on mern stack (Express.js / MongoDb / React / Redux / AntD) with generic crud and auth , admin management .
+
+**Live App Demo** : [https://antd-admin.idurar.net/](https://antd-admin.idurar.net/)
+
+`username : admin@demon.com
+password : admin123`
+
+
+
+## App Features :
+
+### Backend :
+
+* The backend is built with node.js , [express.js Framework](https://expressjs.com/) ,and MongoDb Database
+* Generic Crud Api (Create / Read / Update / Delete)
+* Admin (User) Management Api
+* Auth by Jwt json web token
+
+### Frontend :
+
+* The Frontend is built with React.js , [Ant Design (Antd)](https://ant.design/), and Redux , Redux-thunk.
+* Generic Crud Component (Module) (Create / Read / Update / Delete)
+* Admin (User) Management Module
+* Auth Component Login / Logout
+* Private Route and Public Route
+* Not Found Page
+* Beautiful UI Dashboard
+
---
+++ setup/setup.js
@@ -0,0 +1,69 @@
+require("dotenv").config({ path: __dirname + "/../.env" });
+const fs = require("fs");
+
+const mongoose = require("mongoose");
+const { db } = require("../models/Admin");
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
+
+// import all of our models - they need to be imported only once
+
+const products = JSON.parse(
+  fs.readFileSync(__dirname + "/product.json", "utf-8")
+);
+
+//console.log(products);
+// async function deleteData() {
+//   console.log("😢😢 Goodbye Data...");
+//   await admin.remove();
+//   console.log(
+//     "Data Deleted. To load sample data, run\n\n\t npm run sample\n\n"
+//   );
+//   process.exit();
+// }
+
+async function loadData() {
+  try {
+    await db.collection('product').insertMany(products);
+    console.log("👍👍👍👍👍👍👍👍 Done!");
+    process.exit();
+  } catch (e) {
+    console.log(
+      "\n👎👎👎👎👎👎👎👎 Error! The Error info is below but if you are importing sample data make sure to drop the existing database first with.\n\n\t npm run blowitallaway\n\n\n"
+    );
+    console.log(e);
+    process.exit();
+  }
+}
+
+async function createAdmin() {
+  try {
+    const Admin = require("../models/Admin");
+    var newAdmin = new Admin();
+    const passwordHash = newAdmin.generateHash("123456");
+    //console.log(passwordHash);
+
+    await new Admin({
+      email: "john@email.com",
+      password: passwordHash,
+      name: "admin",
+      surname: "demo",
+    }).save();
+    console.log("👍👍👍👍👍👍👍👍 Admin created : Done!");
+    process.exit();
+  } catch (e) {
+    console.log("\n👎👎👎👎👎👎👎👎 Error! The Error info is below");
+    console.log(e);
+    process.exit();
+  }
+}
+
+createAdmin();
+loadData();
+//  if (process.argv.includes("--delete")) {
+//   deleteData();
+//  } else {
+//   deleteData();
+
+//   //   loadData();
+//  }
