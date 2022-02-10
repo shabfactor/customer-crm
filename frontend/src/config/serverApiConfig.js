f5559252585b26be8936057149741528d29697a5
Rey Angelo Suela <rsuela@gmail.com>
2022-02-10 14:21:29 +1300

    added workflows for ac deployment

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,3 +1,3 @@
 //export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
-export const API_BASE_URL = "http://localhost:8888/api/";
+export const API_BASE_URL = "https://customer-crm-api.app.ap.assurity.cloud/api/";
 export const ACCESS_TOKEN_NAME = "x-auth-token";
