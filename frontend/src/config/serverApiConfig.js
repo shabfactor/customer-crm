4e0f6e249b8386b8dcd1d366f59557d00c4ce3b0
Shaibal chakraborty <shaibal.chakraborty@merlot.aero>
2022-02-14 17:19:02 +1300

    change the service to work in cloud

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -1,5 +1,5 @@
 //export const API_BASE_URL = "https://starter-mern.herokuapp.com/api/";
-//export const API_BASE_URL = "https://customer-crm-api.app.ap.assurity.cloud/api/";
-export const API_BASE_URL = "http://localhost:8888/api/";                               //to run on localhost
+export const API_BASE_URL = "https://customer-crm-api.app.ap.assurity.cloud/api/";
+//export const API_BASE_URL = "http://localhost:8888/api/";                               //to run on localhost
 
 export const ACCESS_TOKEN_NAME = "x-auth-token";
